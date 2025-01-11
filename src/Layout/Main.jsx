7f388675a211d65107002dbcbea3ee1dddce1f23
Md Atikuzzaman <akash203037@gmail.com>
2025-01-11 23:27:32 +0600

    a modular file added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar";
 import { useContext } from "react";
 import { ApplicationContext } from "../contexts/ApplicationSharedContext";
+import MobileNav from "../Shared/MobileNav";
 
 const Main = () => {
   const { showMobileNav } = useContext(ApplicationContext);
@@ -52,47 +53,7 @@ const Main = () => {
               </ul>
             </div>
           </div>
-          {showMobileNav && (
-            <div className="sticky top-10 left-0 md:hidden bg-white">
-              <div className="p-4">
-                <h2 className="text-xl font-bold mb-4">Menu</h2>
-                <ul className="space-y-2">
-                  <li>
-                    <a
-                      href="/"
-                      className="block py-2 px-4 rounded hover:bg-base-300"
-                    >
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/about"
-                      className="block py-2 px-4 rounded hover:bg-base-300"
-                    >
-                      About
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/contact"
-                      className="block py-2 px-4 rounded hover:bg-base-300"
-                    >
-                      Contact
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="/join"
-                      className="block py-2 px-4 rounded hover:bg-base-300"
-                    >
-                      Join
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          )}
+          {showMobileNav && <MobileNav />}
         </div>
         <div>
           <Outlet></Outlet>
