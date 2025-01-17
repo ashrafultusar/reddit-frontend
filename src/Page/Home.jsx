a95794cc9e41cdcd5416ad08e2fb61d778ff4d27
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-17 23:06:20 +0600

    added update

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -4,6 +4,15 @@ import Post from "../Component/Post";
 const Home = () => {
   return (
     <div className="">
+      <div className="flex justify-between mb-12">
+        <p className="text-black text-2xl font-bold">All Posts</p>
+        <div className="flex items-center justify-center gap-4">
+          <p className="bg-[#dcdcdc] text-black px-3 py-1 rounded-md">Newest</p>
+          <p className="bg-[#dcdcdc] text-black px-3 py-1 rounded-md">Oldest</p>
+          <p className="bg-[#dcdcdc] text-black px-3 py-1 rounded-md">Active</p>
+        </div>
+      </div>
+      <hr className="bg-black h-[2px] mb-6" />
       <Post />
     </div>
   );
