73de88a3c9352f2a6d6de4e0f2030783d8b10e87
ashrafultusar <ashrafulislamtusar@gmail.com>
2025-01-20 21:21:48 +0600

    added update

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,18 +1,23 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBlzfXBUUNqtmqh-k65Teswrwzz8UeaBUc",
-  authDomain: "reddit-58aab.firebaseapp.com",
-  projectId: "reddit-58aab",
-  storageBucket: "reddit-58aab.firebasestorage.app",
-  messagingSenderId: "642249173835",
-  appId: "1:642249173835:web:e80ae09c68c7be013f56ce"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth
